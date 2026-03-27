5ca8fb7d7a65702178d733b2477d538fe9e67e67
sequent-hub <sequent@list.ru>
2026-03-27 15:07:26 +0300

    добавлен новый метод получения мудборда

--- src/Routes/api_v2.php
+++ src/Routes/api_v2.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('api/v2')->group(function () {
     Route::prefix('moodboard')->group(function () {
         // V2: history-first API
+        Route::get('/{moodboard_id}/{version?}', [MoodBoardController::class, 'moodboardLoad']);
         Route::post('/metadata/save', [MoodBoardController::class, 'moodboardMetaSave']);
         Route::post('/history/save', [MoodBoardController::class, 'historySave']);
     });
