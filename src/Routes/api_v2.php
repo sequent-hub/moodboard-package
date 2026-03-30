c6db9b6709fa896da30ea6ce522e5ee07591f598
sequent-hub <sequent@list.ru>
2026-03-30 14:43:00 +0300

    добавлены новые роуты v2 для сохранения картинок и файлов

--- src/Routes/api_v2.php
+++ src/Routes/api_v2.php
@@ -1,6 +1,8 @@
 <?php
 
 use Futurello\MoodBoard\Http\Controllers\MoodBoardController;
+use Futurello\MoodBoard\Http\Controllers\ImageController;
+use Futurello\MoodBoard\Http\Controllers\FileController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api/v2')->group(function () {
@@ -9,5 +11,17 @@ Route::prefix('api/v2')->group(function () {
         Route::get('/{moodboard_id}/{version?}', [MoodBoardController::class, 'moodboardLoad']);
         Route::post('/metadata/save', [MoodBoardController::class, 'moodboardMetaSave']);
         Route::post('/history/save', [MoodBoardController::class, 'historySave']);
+    });
+
+    Route::prefix('images')->group(function () {
+        Route::post('/upload', [ImageController::class, 'upload']);
+        Route::get('/{imageId}', [ImageController::class, 'show']);
+        Route::get('/{imageId}/download', [ImageController::class, 'file']);
+    });
+
+    Route::prefix('files')->group(function () {
+        Route::post('/upload', [FileController::class, 'upload']);
+        Route::get('/{fileId}', [FileController::class, 'show']);
+        Route::get('/{fileId}/download', [FileController::class, 'download']);
     });
 });
